a751a139dcb66da6121eeb9de7d1387c7e59a895
Antithesis <zeranexhercy@gmail.com>
2026-06-10 17:36:09 +0800

    v0.0.2

--- src/graycc.cpp
+++ src/graycc.cpp
@@ -8,10 +8,28 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
+    char* p = argv[1];
+
     std::println(".intel_syntax noprefix");
     std::println(".globl main");
     std::println();
     std::println("main:");
-    std::println("    mov rax, {}", args[1]);
+    std::println("    mov rax, {}", std::strtol(p, &p, 10));
+
+    while (*p) {
+        if (*p == '+') {
+            ++p;
+            std::println("    add rax, {}", std::strtol(p, &p, 10));
+            continue;
+        }
+        if (*p == '-') {
+            ++p;
+            std::println("    sub rax, {}", std::strtol(p, &p, 10));
+            continue;
+        }
+        std::println("Invalid char: '{}'", *p);
+        return 1;
+    }
+
     std::println("    ret");
 }
