2bb12c366d8ef779046bb446b9816d926eb91abd
Antithesis <zeranexhercy@gmail.com>
2026-06-10 08:41:15 +0800

    test file

---
+++ test/test1.sh
@@ -0,0 +1,22 @@
+#!/bin/zsh
+assert() {
+    expected="$1"
+    input="$2"
+
+    ../bin/graycc "$input" > tmp.s
+    clang -fuse-ld=lld -o tmp tmp.s
+    ./tmp
+    actual="$?"
+
+    if [ "$actual" = "$expected" ]; then
+        echo "$input => $actual"
+    else
+        echo "$input => $expected expected, but not $actual"
+        exit 1
+    fi
+}
+
+assert 0 0
+assert 42 42
+
+echo OK
