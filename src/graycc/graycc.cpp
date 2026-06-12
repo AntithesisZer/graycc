618cc8c5ca293954117d70ff4b723d573879cf5f
Antithesis <zeranexhercy@gmail.com>
2026-06-12 15:46:04 +0800

    graycc

--- src/graycc/graycc.cpp
+++ src/graycc/graycc.cpp
@@ -1,34 +1,54 @@
 #include <print>
 #include <span>
+#include <string_view>
+#include <vector>
+
+#include <Token.hpp>
 
 int main(int argc, char* argv[]) {
-    std::span<char*> args = std::span(argv, argc);
+    std::span<const char* const> args = std::span(argv, argc);
     if (argc != 2) {
-        std::println(stderr, "Usage: {} <num>", args[0]);
-        return 1;
+        std::println(stderr, "Usage: {} <expression>", args[0]);
+        std::exit(1);
     }
 
-    char* p = argv[1];
+    std::vector<Token> tokens = Tokenize(args[1]);
 
     std::println(".intel_syntax noprefix");
     std::println(".globl main");
     std::println();
     std::println("main:");
-    std::println("    mov rax, {}", std::strtol(p, &p, 10));
 
-    while (*p) {
-        if (*p == '+') {
-            ++p;
-            std::println("    add rax, {}", std::strtol(p, &p, 10));
-            continue;
+    if (tokens.empty() || tokens[0].kind != TokenKind::TK_NUM) {
+        std::println(stderr, "Error: Expression must start with a number");
+        std::exit(1);
+    }
+    std::println("    mov rax, {}", tokens[0].val);
+
+    for (std::size_t i = 1; i < tokens.size(); /**/) {
+        const Token& tok = tokens[i];
+
+        if (tok.kind == TokenKind::TK_EOF) {
+            break;
         }
-        if (*p == '-') {
-            ++p;
-            std::println("    sub rax, {}", std::strtol(p, &p, 10));
-            continue;
+
+        if (tok.kind != TokenKind::TK_RESERVED) {
+            std::println(stderr, "Error: Expected operator '{}'", tok.loc);
+            std::exit(1);
         }
-        std::println("Invalid char: '{}'", *p);
-        return 1;
+
+        if (i + 1 > tokens.size() || tokens[i + 1].kind != TokenKind::TK_NUM) {
+            std::println(stderr, "Error: Expected number after operator '{}'", tokens[i + 1].loc);
+            std::exit(1);
+        }
+
+        if (const Token& next_tok = tokens[i + 1]; tok.loc == "+") {
+            std::println("    add rax, {}", next_tok.val);
+        } else if (tok.loc == "-") {
+            std::println("    sub rax, {}", next_tok.val);
+        }
+
+        i += 2;
     }
 
     std::println("    ret");
