49e9684b91bf96eb8e135af63edd982f86033d07
Antithesis <zeranexhercy@gmail.com>
2026-06-13 14:26:26 +0800

    AST

--- libs/AST/include/AST.hpp
+++ libs/AST/include/AST.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstdint>
+#include <memory_resource>
 
 enum class NodeKind : std::uint64_t {
     ND_ADD,
@@ -22,17 +23,30 @@ struct alignas(32) Node {
     };
 };
 
-inline auto alloc_binary_node(NodeKind kind, Node* lhs, Node* rhs) -> Node* {
-    return new Node{
-        .kind = kind,
-        .lhs = lhs,
-        .rhs = rhs,
-    };
-}
+class ASTContext {
+public:
+    ASTContext() : m_pool(std::pmr::new_delete_resource()), m_alloc(&m_pool) {}
+    ASTContext(const ASTContext&) = delete;
+    auto operator=(const ASTContext&) -> ASTContext& = delete;
+    ~ASTContext() = default;
 
-inline auto alloc_num_node(std::uint64_t val) -> Node* {
-    return new Node{
-        .kind = NodeKind::ND_NUM,
-        .val = val,
-    };
-}
+    auto make_num(std::uint64_t val) -> Node* {
+        void* mem = m_alloc.allocate(1);
+        return ::new (mem) Node{
+            .kind = NodeKind::ND_NUM,
+            .val = val,
+        };
+    }
+    auto make_binary(NodeKind kind, Node* lhs, Node* rhs) -> Node* {
+        void* mem = m_alloc.allocate(1);
+        return ::new (mem) Node{
+            .kind = kind,
+            .lhs = lhs,
+            .rhs = rhs,
+        };
+    }
+
+private:
+    std::pmr::monotonic_buffer_resource m_pool;
+    std::pmr::polymorphic_allocator<Node> m_alloc;
+};
