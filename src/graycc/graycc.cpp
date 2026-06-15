08bbf9b8296f2e0edd6811dc9679acf8fb55f0b4
Antithesis <zeranexhercy@gmail.com>
2026-06-15 14:47:48 +0800

    graycc

--- src/graycc/graycc.cpp
+++ src/graycc/graycc.cpp
@@ -31,33 +31,4 @@ int main(int argc, char* argv[]) {
 
     std::println("    pop rax");
     std::println("    ret");
-
-    // if (tokens.empty() || tokens[0].kind != TokenKind::TK_NUM) {
-    //     error_at(tokens[0].loc, expression, "Expression must start with a number");
-    // }
-    // std::println("    mov rax, {}", tokens[0].val);
-
-    // for (std::size_t i = 1; i < tokens.size(); /**/) {
-    //     const Token& tok = tokens[i];
-
-    //     if (tok.kind == TokenKind::TK_EOF) {
-    //         break;
-    //     }
-
-    //     if (tok.kind != TokenKind::TK_RESERVED) {
-    //         error_at(tokens[i].loc, expression, "Expected operator");
-    //     }
-
-    //     if (i + 1 > tokens.size() || tokens[i + 1].kind != TokenKind::TK_NUM) {
-    //         error_at(tokens[i + 1].loc, expression, "Expected number after operator");
-    //     }
-
-    //     if (const Token& next_tok = tokens[i + 1]; tok.loc == "+") {
-    //         std::println("    add rax, {}", next_tok.val);
-    //     } else if (tok.loc == "-") {
-    //         std::println("    sub rax, {}", next_tok.val);
-    //     }
-
-    //     i += 2;
-    // }
 }
